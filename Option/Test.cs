1cb37bde8d93b2d49a8ecf29936df7ff4a37ac51
DESKTOP-SHMNK1V\shita <hirotoshinote@gmail.com>
2021-08-29 02:45:53 +0900

    初期テスト

---
+++ Option/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    DontDestroyObj DDO;
+    VH1 VH1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VH1 = GameObject.Find("DontDestroyObj").GetComponent<VH1>();
+        VH1.a = 1;
+        Debug.Log(VH1.b+"テストの方だよ");
+        VH1.b = "Option";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
